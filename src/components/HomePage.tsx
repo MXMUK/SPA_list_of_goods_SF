8384a0c55f266f23cf448fb20e72774ae0b8c02e
Roman Maksymuk <romamaksymuk@gmail.com>
2023-03-28 14:19:43 +0300

    fix-2

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -15,6 +15,7 @@ export const HomePage: FC = memo(() => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
+
   const dispatch = useDispatch();
 
   const loadGoods = useCallback(async () => {
@@ -51,7 +52,15 @@ export const HomePage: FC = memo(() => {
             </span>
           </div>
 
-          <div className="block">{true ? <GoodsList isLoading={isLoading} /> : <Loader />}</div>
+          <div className="block">
+            {true ? (
+              <GoodsList
+                isLoading={isLoading}
+              />
+            ) : (
+              <Loader />
+            )}
+          </div>
         </div>
       </div>
 
